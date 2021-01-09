ca399e77484f5c40dda82a65af63aa14e5032f47
nagalakshmi-palyam <nagalakshmi.palyam@gmail.com>
2021-01-09 09:04:05 +0530

    # Account Fragments Added

---
+++ app/src/main/java/com/lakshmi/myapplication/Fragmets/PrivacyFragment.kt
@@ -0,0 +1,33 @@
+package com.lakshmi.myapplication.Fragmets
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import androidx.fragment.app.Fragment
+import com.lakshmi.myapplication.R
+import kotlinx.android.synthetic.main.fragment_privacy.*
+
+class PrivacyFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_privacy, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initviews(view)
+    }
+    fun initviews(view: View){
+        webview.loadUrl("https://sharemystore.com/privacy-policy")
+    }
+}
